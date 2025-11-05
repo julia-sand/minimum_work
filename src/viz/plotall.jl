4f309dc2edbae8f0ce6e7a18355b060132641a57
julia-sand <73934921+julia-sand@users.noreply.github.com>
2025-11-05 11:30:55 +0200

    Modify make_plot to exclude indirect data
    Commented out the indirect data processing and plotting.

--- src/viz/plotall.jl
+++ src/viz/plotall.jl
@@ -1,14 +1,17 @@
 using Plots,CSV,DataFrames
 
 
-function make_plot(filename_direct,filename_indirect)
+function make_plot(filename_direct)#,filename_indirect)
     
     df_direct = DataFrame(CSV.File(filename_direct))
-    df_indirect = DataFrame(CSV.File(filename_indirect))
+    #df_indirect = DataFrame(CSV.File(filename_indirect))
+    
+    p_mean = plot(df_direct.t, df_direct.x1)
+    p_kappa = plot(df_direct.t, df_direct.kappa)
 
     
-    p_mean = plot([df_direct.t,df_indirect.t], [df_direct.x1,df_indirect.x1])
-    p_kappa = plot([df_direct.t,df_indirect.t], [df_direct.kappa,df_indirect.kappa])
+    #p_mean = plot([df_direct.t,df_indirect.t], [df_direct.x1,df_indirect.x1])
+    #p_kappa = plot([df_direct.t,df_indirect.t], [df_direct.kappa,df_indirect.kappa])
 
     # Create subplots
     plot_out = plot(p1,p2,
