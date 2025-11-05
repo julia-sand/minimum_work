a332449eaa577071dafa2b2914072a0a37ce052a
julia-sand <73934921+julia-sand@users.noreply.github.com>
2025-11-05 11:28:57 +0200

    remove indirect method

--- src/integrate.jl
+++ src/integrate.jl
@@ -5,17 +5,17 @@ const g = 0.01
 const mu0 = 0
 const muT = 2
 
-filename_indirect = "results/indirect.csv"
+#filename_indirect = "results/indirect.csv"
 filename_direct = "results/direct.csv"
 
 #import scripts
 include("direct.jl")
-include("firstorder.jl")
+#include("firstorder.jl")
 include("viz/plotall.jl")
 
 #integrate both models
 solve_direct_equil(T,g,mu0,muT,filename_direct)
-solve_indirect_equil(T,g,mu0,muT,filename_indirect)
+#solve_indirect_equil(T,g,mu0,muT,filename_indirect)
 
 #make plots
-make_plot(filename_direct,filename_indirect)
+make_plot(filename_direct)
